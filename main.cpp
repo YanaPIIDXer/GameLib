07533e697b364cc9ae5daa2211aef9ea2b28a024
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-02-25 17:21:28 +0900

    エントリポイントの修正。

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
-int main()
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE phInst, LPSTR lpCmd, int nCmd)
 {
+	MessageBox(nullptr, "‚×‚ë‚×‚ë‚Î‚Ÿ", "GameLib", MB_OK);
 	return 0;
 }
