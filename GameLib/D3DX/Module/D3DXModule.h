7485b4526d86aaf016785a7e7c0786659abbe541
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-06-22 11:07:17 +0900

    D3DXのモジュール用インタフェースを定義。

---
+++ GameLib/D3DX/Module/D3DXModule.h
@@ -0,0 +1,19 @@
+#ifndef D3DXMODULE_H
+#define D3DXMODULE_H
+
+/**
+ * @class ID3DXModule
+ * @brief D3DXの機能モジュールインタフェース
+ */
+class ID3DXModule
+{
+public:
+
+	/**
+	 * @brief デストラクタ
+	 */
+	virtual ~ID3DXModule() = 0;
+
+};
+
+#endif		// #ifndef D3DXMODULE_H
