1164b816cc2ae6fabf6dac440a71c678d1de68cb
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-06-22 14:05:24 +0900

    乗算用のオペレータオーバーロードを実装。

--- GameLib/Math/Matrix.h
+++ GameLib/Math/Matrix.h
@@ -35,6 +35,31 @@ public:
 	 */
 	const D3DXMATRIX Get() const { return Mat; }
 
+public:		// オペレータオーバーロード
+
+	/**
+	 * @fn Matrix operator *(const Matrix &Arg)
+	 * @brief 乗算
+	 * @param[in] Arg 行列
+	 * @return 乗算結果
+	 */
+	Matrix operator *(const Matrix &Arg) const
+	{
+		Matrix Value;
+		Value.Mat = Mat * Arg.Mat;
+		return Value;
+	}
+
+	/**
+	 * @fn void operator *=(const Matrix &Arg)
+	 * @brief 自身に乗算
+	 * @param[in] Arg 行列
+	 */
+	void operator *=(const Matrix &Arg)
+	{
+		Mat *= Arg.Mat;
+	}
+
 private:
 
 	// 行列
